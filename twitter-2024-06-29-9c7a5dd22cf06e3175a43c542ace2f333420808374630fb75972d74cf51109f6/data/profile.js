6e6a75e073b84af5c5b10f803b0ac0eebbf6c3f4
UzuheMayuri1 <uzuhemayuri@gmail.com>
2024-08-07 10:53:06 +0900

    Add files via upload

---
+++ twitter-2024-06-29-9c7a5dd22cf06e3175a43c542ace2f333420808374630fb75972d74cf51109f6/data/profile.js
@@ -0,0 +1,13 @@
+window.YTD.profile.part0 = [
+  {
+    "profile" : {
+      "description" : {
+        "bio" : "古いiOSと夢グループが好きな中3クソガキ。📱S22/4✕24S🎧K702,EarPods フォロバ適当。 @_TadaHima @Uzuhe_0630 #今日のバ持ち",
+        "website" : "https://t.co/IElw3QsIXH",
+        "location" : "iOS6 Town"
+      },
+      "avatarMediaUrl" : "https://pbs.twimg.com/profile_images/1784975876817620992/gnGhSE0t.jpg",
+      "headerMediaUrl" : "https://pbs.twimg.com/profile_banners/1721114001562886144/1717505265"
+    }
+  }
+]
---
+++ twitter-2024-06-29-9c7a5dd22cf06e3175a43c542ace2f333420808374630fb75972d74cf51109f6/data/verified.js
@@ -0,0 +1,8 @@
+window.YTD.verified.part0 = [
+  {
+    "verified" : {
+      "accountId" : "1721114001562886144",
+      "verified" : false
+    }
+  }
+]
