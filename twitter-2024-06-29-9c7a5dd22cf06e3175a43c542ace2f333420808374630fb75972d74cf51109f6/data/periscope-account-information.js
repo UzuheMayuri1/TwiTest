296eeeb70da9b6337b08b79c3df08c1360451c0c
UzuheMayuri1 <uzuhemayuri@gmail.com>
2024-08-07 10:51:10 +0900

    Add files via upload

---
+++ twitter-2024-06-29-9c7a5dd22cf06e3175a43c542ace2f333420808374630fb75972d74cf51109f6/data/periscope-account-information.js
@@ -0,0 +1,14 @@
+window.YTD.periscope_account_information.part0 = [
+  {
+    "periscopeAccountInformation" : {
+      "displayName" : "ぼうぴた🇯🇵",
+      "digitsId" : "",
+      "username" : "_TadaHima",
+      "twitterId" : "1721114001562886144",
+      "id" : "1VGQvVqVvzJQO",
+      "twitterScreenName" : "_TadaHima_",
+      "isTwitterUser" : true,
+      "createdAt" : "2023-11-09T08:26:26.23612817Z"
+    }
+  }
+]
